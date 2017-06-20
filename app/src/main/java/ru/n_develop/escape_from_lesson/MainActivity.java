6cf3c79dd74b791904c5e21652e8f86a7a6fcb2b
DIMA_N <dim9044@yandex.ru>
2017-06-21 00:16:52 +0300

    Приложение готово

--- app/src/main/java/ru/n_develop/escape_from_lesson/MainActivity.java
+++ app/src/main/java/ru/n_develop/escape_from_lesson/MainActivity.java
@@ -7,9 +7,8 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
+
 
 import java.util.Random;
 
@@ -18,65 +17,52 @@ public class MainActivity extends AppCompatActivity
 
 	TextView textView;
 
-	private ImageView mImageView;
-	private Animation mFadeInAnimation, mFadeOutAnimation;
+	private ImageView escapeImageView;
+	private ImageView notEscapeImageView;
+	private Animation mFadeInAnimation;
+
+	private Boolean escapeBool;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
+
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		textView = (TextView) findViewById(R.id.textView);
 
-		mImageView = (ImageView) findViewById(R.id.imageView);
+		escapeImageView = (ImageView) findViewById(R.id.escape);
+		notEscapeImageView = (ImageView) findViewById(R.id.notescape);
 
 		// подключаем файл анимации
 		mFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.alphain);
-		mFadeOutAnimation = AnimationUtils.loadAnimation(this, R.anim.alphaout);
 		mFadeInAnimation.setAnimationListener(animationFadeInListener);
-		mFadeOutAnimation.setAnimationListener(animationFadeOutListener);
 
 	}
 
 	public void onClick (View view)
 	{
 
-
-		// при запуске начинаем с анимации исчезновения
-		mImageView.startAnimation(mFadeInAnimation);
-
-		textView.setText(escape());
-
-
+		escapeImageView.setVisibility(View.GONE);
+		notEscapeImageView.setVisibility(View.GONE);
+		escape();
 	}
-
-
-	Animation.AnimationListener animationFadeOutListener = new Animation.AnimationListener() {
-
-		@Override
-		public void onAnimationEnd(Animation animation) {
-//			mImageView.startAnimation(mFadeInAnimation);
-//			mImageView.setVisibility(View.GONE);
-		}
-
-		@Override
-		public void onAnimationRepeat(Animation animation) {
-			// TODO Auto-generated method stub
-		}
-
-		@Override
-		public void onAnimationStart(Animation animation) {
-			// TODO Auto-generated method stub
-		}
-	};
-
 
 	Animation.AnimationListener animationFadeInListener = new Animation.AnimationListener() {
 		@Override
 		public void onAnimationEnd(Animation animation) {
-//			mImageView.startAnimation(mFadeOutAnimation);
-//			mImageView.setVisibility(View.VISIBLE);
+			if (escapeBool)
+			{
+				notEscapeImageView.setVisibility(View.GONE);
+				escapeImageView.setVisibility(View.VISIBLE);
+			}
+			else
+			{
+				escapeImageView.setVisibility(View.GONE);
+				notEscapeImageView.setVisibility(View.VISIBLE);
+			}
 		}
 
 		@Override
@@ -87,13 +73,22 @@ public class MainActivity extends AppCompatActivity
 		@Override
 		public void onAnimationStart(Animation animation) {
 			// TODO Auto-generated method stub
-			mImageView.setVisibility(View.VISIBLE);
+			if (escapeBool)
+			{
+				notEscapeImageView.setVisibility(View.GONE);
+				escapeImageView.setVisibility(View.VISIBLE);
+			}
+			else
+			{
+				escapeImageView.setVisibility(View.GONE);
+				notEscapeImageView.setVisibility(View.VISIBLE);
+			}
 
 		}
 	};
 
 
-	private String escape ()
+	private void escape ()
 	{
 		int escape = 0;
 		int notEscape = 0;
@@ -105,26 +100,95 @@ public class MainActivity extends AppCompatActivity
 			else notEscape++;
 		}
 
-		String result;
+		Random rand = new Random();
+
 		if (escape > notEscape)
 		{
-			result = "ребята удачи вам валите из этой дыры";
-			result = "погода норм пойдем гулять";
-			result = "го за шурмой";
-			result = "го в кино";
-			result = "го бахнем";
-			result = "валим валим валим";
+			notEscapeImageView.setVisibility(View.GONE);
+			notEscapeImageView.clearAnimation();
+			escapeBool = true;
+			int r = rand.nextInt(120)+1;
+			if (r < 20)
+			{
+				escapeImageView.setImageResource(R.drawable.escape1);
+			}
+			else if (r > 20 && r < 40)
+			{
+				escapeImageView.setImageResource(R.drawable.escape2);
+
+			}
+			else if (r > 40 && r < 60)
+			{
+				escapeImageView.setImageResource(R.drawable.escape3);
+
+			}
+			else if (r > 60 && r < 80)
+			{
+				escapeImageView.setImageResource(R.drawable.escape4);
+
+			}
+			else if (r > 80 && r < 100)
+			{
+				escapeImageView.setImageResource(R.drawable.escape5);
+
+			}
+			else if (r > 100 && r < 120)
+			{
+				escapeImageView.setImageResource(R.drawable.escape6);
+
+			}
+
+			// при запуске начинаем с анимации исчезновения
+			escapeImageView.setVisibility(View.VISIBLE);
+			escapeImageView.startAnimation(mFadeInAnimation);
+
+
+//			result = "ребята, удачи вам, валите из этой дыры";
+//			result = "погода норм, пойдем гулять";
+//			result = "го за шаурмой";
+//			result = "го в кино";
+//			result = "го бахнем";
+//			result = "валим-валим-валим";
 		}
 		else
 		{
-			result = "к сожалению остаемся на паре";
-			result = "сегодня не наш день. оставемся";
-			result = "сиди";
-			result = "неа";
-			result = "неудачный момент чтобы слиться";
-		}
+			escapeImageView.setVisibility(View.GONE);
+			escapeImageView.clearAnimation();
 
-		return result;
+			escapeBool = false;
+
+			int r = rand.nextInt(100)+1;
+			if (r < 20)
+			{
+				notEscapeImageView.setImageResource(R.drawable.notescape1);
+			}
+			else if (r > 20 && r < 40)
+			{
+				notEscapeImageView.setImageResource(R.drawable.notescape2);
+
+			}
+			else if (r > 40 && r < 60)
+			{
+				notEscapeImageView.setImageResource(R.drawable.notescape3);
+
+			}
+			else if (r > 60 && r < 80)
+			{
+				notEscapeImageView.setImageResource(R.drawable.notescape4);
+
+			}else if (r > 80 && r < 100)
+			{
+				notEscapeImageView.setImageResource(R.drawable.notescape5);
+
+			}
+			notEscapeImageView.setVisibility(View.VISIBLE);
+			notEscapeImageView.startAnimation(mFadeInAnimation);
+//			result = "к сожалению, остаемся на паре";
+//			result = "сегодня не наш день. остаемся";
+//			result = "сиди";
+//			result = "неа";
+//			result = "неудачный момент, чтобы слиться";
+		}
 	}
 
 
