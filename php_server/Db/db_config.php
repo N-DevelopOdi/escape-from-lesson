c3cd8d6ee4041615aae1138fd746855ce8b1a9cd
dim9044 <dim9044@yandex.ru>
2017-12-13 19:03:57 +0300

    Delete db_config.php

--- php_server/Db/db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	define('DB_USER', "u767569555_user"); //логин админа БД
-    define('DB_PASSWORD', "x1NHvUAUfX9F"); // пароль админа БД
-    define('DB_DATABASE', "u767569555_escap"); // база данных
-    define('DB_SERVER', "mysql.hostinger.ru"); // сервер 'localhost'
-?>
